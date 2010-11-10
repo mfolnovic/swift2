b27852fab63b1f769f5fa787b4674c3bbfe80e8c
Matija Folnovic <matijafolnovic@gmail.com>
2010-11-10 12:20:20 +0100

    Documentation for Request

--- src/HTTP/Request/Request.php
+++ src/HTTP/Request/Request.php
@@ -22,15 +22,43 @@ use Utilities\Knapsack as Knapsack;
  */
 
 class Request {
+	/**
+	 * $_GET
+	 */
 	public $get;
+	/**
+	 * $_POST
+	 */
 	public $post;
+	/**
+	 * $_COOKIES
+	 */
 	public $cookies;
+	/**
+	 * $_FILES
+	 */
 	public $files;
+	/**
+	 * $_SERVER
+	 */
 	public $server;
 
-	private $url;
+	/**
+	 * HTTP Status Code
+	 */
 	private $code = 200;
 
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 * @param  array $get     $_GET
+	 * @param  array $post    $_POST
+	 * @param  array $cookies $_COOKIES
+	 * @param  array $files   $_FILES
+	 * @param  array $server  $_SERVER
+	 * @return void
+	 */
 	public function __construct($get = array(), $post = array(), $cookies = array(), $files = array(), $server = array()) {
 		$this -> get     = new Knapsack($get);
 		$this -> post    = new Knapsack($post);
@@ -39,14 +67,33 @@ class Request {
 		$this -> server  = new Knapsack($server);
 	}
 
+	/**
+	 * Returns current HTTP Status Code
+	 *
+	 * @access public
+	 * @return int
+	 */
 	public function getStatusCode() {
 		return $this -> code;
 	}
 
+	/**
+	 * Changes HTTP Status Code to $code
+	 *
+	 * @access public
+	 * @param  int $code New HTTP Status Code
+	 * @return void
+	 */
 	public function setStatusCode($code) {
 		$this -> code = (int)$code;
 	}
 
+	/**
+	 * Returns Request's URL
+	 *
+	 * @access public
+	 * @return string
+	 */
 	public function getUrl() {
 		if(isset($server -> request_url)) {
 			return $server -> request_url;
