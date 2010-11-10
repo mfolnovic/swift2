bd918c0c345e8f60808978e3c03be74120d8047f
Matija Folnovic <matijafolnovic@gmail.com>
2010-11-10 12:10:09 +0100

    Add new Knapsack class - used for storing data (currently only in Request -> get, post etc.)

---
+++ src/Utilities/Knapsack.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Swift
+ *
+ * @author    Matija Folnovic
+ * @copyright Copyright (c), Matija Folnovic <matijafolnovic@gmail.com>
+ * @license   GPLv3
+ * @package   Swift
+ */
+
+namespace Utilities;
+
+/**
+ * Knapsack
+ *
+ * Knapsack is used for storing some data
+ *
+ * @author     Matija Folnovic
+ * @package    Swift
+ * @subpackage Utilities
+ */
+
+class Knapsack {
+	var $knapsack = array();
+
+	function __construct($data = array()) {
+		$this -> knapsack = (array)$data;
+	}
+
+	function get($index) {
+		return $this -> knapsack[$index];
+	}
+
+	function __get($index) {
+		return $this -> get($index);
+	}
+
+	function set($index, $value) {
+		return $this -> knapsack[$index] = $value;
+	}
+
+	function __set($index, $value) {
+		$this -> set($index, $value);
+	}
+}
+
+?>
