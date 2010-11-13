189bb7042bfa5e84206f4036ab51ad9f89b6b545
Matija Folnovic <matijafolnovic@gmail.com>
2010-11-13 16:13:16 +0100

    Test getUrl

--- tests/HTTP/Request/RequestTest.php
+++ tests/HTTP/Request/RequestTest.php
@@ -20,6 +20,15 @@ class RequestTest extends \PHPUnit_Framework_TestCase {
 		$request -> setStatusCode('500');
 		$this -> assertEquals($request -> getStatusCode(), 500);
 	}
+
+	public function testGetUrl() {
+		$request = new Request(array(),array(),array(),array(),array('REQUEST_URI' => '/index.php'));
+
+		$this -> assertEquals($request -> getUrl(), '/index.php');
+
+		$request = new Request();
+		$this -> assertEquals($request -> getUrl(), '/');
+	}
 }
 
 ?>
