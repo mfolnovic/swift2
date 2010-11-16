a5d0b0d1ed1fa885edcaa66f444a317d88162c65
Matija Folnovic <matijafolnovic@gmail.com>
2010-11-16 21:22:03 +0100

    index.php => now, used for testing in browser

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+define("APP_DIR", __DIR__ . '/app/');
+
+include './src/Loader/Loader.php';
+
+Loader\Loader::addNamespace('src', __DIR__ . '/src/');
+Loader\Loader::addNamespace('controller', __DIR__ . '/app/controllers/');
+
+$request = new HTTP\Request\Request;
+$request -> run();
+
+$response = new HTTP\Response\Response;
+
+$controller = new HTTP\Controller\Controller($request, $response);
+
+?>
