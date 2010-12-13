836dcd5824b8e44e41359842227e41f7acb9676b
Matija Folnovic <matijafolnovic@gmail.com>
2010-12-13 22:47:43 +0100

    Coding style in index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ include './src/loader/loader.php';
 Swift\Loader\Loader::addNamespace('Swift', __DIR__ . '/src/');
 Swift\Loader\Loader::addNamespace('Application', __DIR__ . '/app/');
 
-$request = new Swift\HTTP\Request\Request;
-$response = new Swift\HTTP\Response\Response;
+$request    = new Swift\HTTP\Request\Request;
+$response   = new Swift\HTTP\Response\Response;
 $controller = new Swift\HTTP\Controller\Controller($request, $response);
 
 $response -> renderLayout($request -> controller_data);
